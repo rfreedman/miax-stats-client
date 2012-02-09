5aa380ebf57e58b172e10eace001601cbda7ba16
Rich Freedman <richfreedman@gmail.com>
2012-02-09 12:08:34 -0500

    replaced firm/cloud radio buttons with tab control, data-bound cloud-tab tables

--- griffon-app/controllers/statsclient/MonitorTabController.groovy
+++ griffon-app/controllers/statsclient/MonitorTabController.groovy
@@ -15,18 +15,13 @@ class MonitorTabController {
         def statsModel = args.statsModel
         statsColumnConfig = args.statsColumnConfig
         
-        // todo - the cloud-level table
-        setupTable(view.statsTable, statsColumnConfig.firmColumns, MonitorTabModel.StatsDataView.FIRM, [0])
-        setupTable(view.detailTable, statsColumnConfig.instanceColumns, MonitorTabModel.StatsDataView.INSTANCE, [0, 1, 2])
+        // firm-tab tables
+        setupTable(view.firmTable, statsColumnConfig.firmColumns, MonitorTabModel.StatsDataView.FIRM, [0])
+        setupTable(view.firmDetailTable, statsColumnConfig.instanceColumns, MonitorTabModel.StatsDataView.INSTANCE, [0, 1, 2])
 
-        view.rbFirm.actionPerformed = { evt ->
-            switchRollup(MonitorTabModel.StatsDataView.FIRM)
-        };
-
-        view.rbCloud.actionPerformed = { evt ->
-            switchRollup(MonitorTabModel.StatsDataView.CLOUD)
-        };
-
+        // cloud-tab tables
+        setupTable(view.cloudTable, statsColumnConfig.cloudColumns, MonitorTabModel.StatsDataView.FIRM, [0])
+        setupTable(view.cloudDetailTable, statsColumnConfig.instanceColumns, MonitorTabModel.StatsDataView.INSTANCE, [0, 1, 2])
 
         statsModel.subscribe(this.model) // subscribe the tab model to the global stats model for this window
     }
@@ -54,17 +49,6 @@ class MonitorTabController {
         TableColumnModel columns = table.getColumnModel();
         columns.each {column ->
             column.preferredWidth = 50
-        }
-    }
-
-
-    void switchRollup(MonitorTabModel.StatsDataView value) {
-        if (model.rollupMode != value) {
-            model.rollupMode = value
-
-            // todo - actually switch the mode
-            String msg = "switched to ${model.rollupMode}"
-            System.out.println(msg)
         }
     }
 }
--- griffon-app/views/statsclient/MonitorTabView.groovy
+++ griffon-app/views/statsclient/MonitorTabView.groovy
@@ -1,43 +1,63 @@
 package statsclient
 
-import java.awt.BorderLayout
 import java.awt.Color
+import java.awt.ComponentOrientation
 import javax.swing.JSplitPane
-import net.miginfocom.swing.MigLayout
 import javax.swing.JTable
+import net.miginfocom.swing.MigLayout
 
 panel(id: 'root', layout: new MigLayout(), constraints: 'w 100%, h 100%') {
 
-    splitPane(id: "splitter",
-            constraints: 'newLine, w 100%, h 100%',
-            orientation: JSplitPane.VERTICAL_SPLIT,
-            resizeWeight: 0.5,
-            oneTouchExpandable: true,
-            continuousLayout: true,
-            border: lineBorder(color: Color.LIGHT_GRAY, thickness: 1)) {
+    tabbedPane(id: 'documents', constraints: 'w 100%, h 100%', componentOrientation: ComponentOrientation.RIGHT_TO_LEFT, selectedIndex:1) {
 
+        // Cloud tab
+        splitPane(id: "Cloud",
+                constraints: 'newLine, w 100%, h 100%',
+                orientation: JSplitPane.VERTICAL_SPLIT,
+                resizeWeight: 0.5,
+                oneTouchExpandable: true,
+                continuousLayout: true,
+                border: lineBorder(color: Color.LIGHT_GRAY, thickness: 1)) {
 
-        panel(layout: new MigLayout(), constraints: 'w 100%, h 100%') {
-            panel(layout: new BorderLayout(), constraints: 'w 100%, h 100%') {
-                panel(layout: new MigLayout("align right"), constraints: "North") {
-                    buttonGroup(id: 'firm-cloud').with { group ->
-                        radioButton(id: 'rbFirm', text: 'Firm', buttonGroup: group, selected: bind {model.rollupMode == MonitorTabModel.StatsDataView.FIRM})
-                        radioButton(id: 'rbCloud', text: 'Cloud', buttonGroup: group, selected: bind {model.rollupMode == MonitorTabModel.StatsDataView.CLOUD})
+            panel(layout: new MigLayout(), constraints: 'w 100%, h 100%') {
+                scrollPane(constraints: 'w 100%, h 100%') {
+                    table(id: 'cloudTable', constraints: 'w 100%, h 100%', autoResizeMode: JTable.AUTO_RESIZE_OFF) {
                     }
                 }
-                scrollPane(constraints: 'Center') {
-                    table(id: 'statsTable', constraints: 'w 100%, h 100%', autoResizeMode:JTable.AUTO_RESIZE_OFF) {
+            }
+
+            panel(layout: new MigLayout(), constraints: 'w 100%, h 100%') {
+                scrollPane(constraints: 'w 100%, h 100%') {
+                    table(id: 'cloudDetailTable', constraints: 'w 100%, h 100%', autoResizeMode: JTable.AUTO_RESIZE_OFF) {
                     }
                 }
             }
         }
 
-        panel(layout: new MigLayout(), constraints: 'w 100%, h 100%') {
-            scrollPane(constraints: 'w 100%, h 100%') {
-                table(id: 'detailTable', constraints: 'w 100%, h 100%', autoResizeMode:JTable.AUTO_RESIZE_OFF) {
+        // Firm tab
+        splitPane(id: "Firm",
+                constraints: 'newLine, w 100%, h 100%',
+                orientation: JSplitPane.VERTICAL_SPLIT,
+                resizeWeight: 0.5,
+                oneTouchExpandable: true,
+                continuousLayout: true,
+                border: lineBorder(color: Color.LIGHT_GRAY, thickness: 1)) {
+
+            panel(layout: new MigLayout(), constraints: 'w 100%, h 100%') {
+                scrollPane(constraints: 'w 100%, h 100%') {
+                    table(id: 'firmTable', constraints: 'w 100%, h 100%', autoResizeMode: JTable.AUTO_RESIZE_OFF) {
+                    }
+                }
+            }
+
+            panel(layout: new MigLayout(), constraints: 'w 100%, h 100%') {
+                scrollPane(constraints: 'w 100%, h 100%') {
+                    table(id: 'firmDetailTable', constraints: 'w 100%, h 100%', autoResizeMode: JTable.AUTO_RESIZE_OFF) {
+                    }
                 }
             }
         }
+
     }
 
 
